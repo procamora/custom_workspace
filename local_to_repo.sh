46808921309b7e219c5c5c770f91f18fc4e86d2b
procamora <pablojoserocamora@gmail.com>
2020-04-14 17:37:31 +0200

    update config

---
+++ local_to_repo.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+
+meld ~/Documents/custom_workspace/bspwm ~/.config/bspwm
+meld ~/Documents/custom_workspace/compton ~/.config/compton
+meld ~/Documents/custom_workspace/sxhkd ~/.config/sxhkd
+meld ~/Documents/custom_workspace/polybar ~/.config/polybar
+meld ~/Documents/custom_workspace/zsh/zshrc ~/.zshrc
