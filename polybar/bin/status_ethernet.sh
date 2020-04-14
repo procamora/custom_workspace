53b33c1e0e5b4c0126d37e4c0e3cbe1a7194f6c4
procamora <pablojoserocamora@gmail.com>
2020-04-14 21:26:07 +0200

    fix errors sxhkd

--- polybar/bin/status_ethernet.sh
+++ polybar/bin/status_ethernet.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
-ETH="enp4s0"
-IFACE=$(/usr/sbin/ip address | grep "$ETH:" | awk '{print $2}' | tr -d ":")
+IFACE=$(ip address show | grep "enp")
 
-if [ "$IFACE" = "$ETH" ]; then
-    echo "%{F#2495E7} %{F#E2EE6A}$(/usr/sbin/ip address show $ETH | grep "inet " | awk '{print $2}' | cut -d/ -f1)%{u-}"
+if [ "$?" -eq "0" ]; then
+    echo "%{F#2495E7}%{u-}"
 else
     echo "%{F#2495E7}%{u-}%{F-}"
 fi
