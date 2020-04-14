c6f6d6ad99a9df25bb7b50ea60749e41c1d9e952
procamora <pablojoserocamora@gmail.com>
2020-04-14 20:56:57 +0200

    update config polybar to notebook

--- polybar/bin/status_wifi.sh
+++ polybar/bin/status_wifi.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
-ETH="wlan0"
-IFACE=$(/usr/sbin/ip address | grep "$ETH:" | awk '{print $2}' | tr -d ":")
+IFACE=$(ip address show | grep "wlp")
 
-if [ "$IFACE" = "$ETH" ]; then
-    echo "%{F#2495e7}直 %{F#e2ee6a}$(/usr/sbin/ip address show $ETH | grep "inet " | awk '{print $2}' | cut -d/ -f1)%{u-}"
+if [ "$?" -eq "0" ]; then
+    echo "%{F#2495e7}直%{u-}"
 else
     echo "%{F#2495e7}睊%{u-}%{F-}"
 fi
