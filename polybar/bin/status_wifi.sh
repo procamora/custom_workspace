a73aebb8328707eb829c7a36941f851e6632df88
procamora <pablojoserocamora@gmail.com>
2020-04-16 17:19:34 +0200

    update conf

--- polybar/bin/status_wifi.sh
+++ polybar/bin/status_wifi.sh
@@ -5,6 +5,6 @@ IFACE=$(ip address show | grep "wlp")
 if [ "$?" -eq "0" ]; then
     echo "%{F#2495e7}直%{u-}"
 else
-    echo "%{F#2495e7}睊%{u-}%{F-}"
+    echo "%{F#ff0000}睊%{u-}%{F-}"
 fi
 
--- polybar/launch.sh
+++ polybar/launch.sh
@@ -4,6 +4,6 @@ killall -q polybar
 
 while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
 
-polybar example &
+polybar my_config &
 
 echo "Polybar launched..."
