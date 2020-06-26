dbdc36e098bafe43e6b7a7db8679bb689e1463f1
procamora <pablojoserocamora@gmail.com>
2020-06-27 00:15:34 +0200

    update script polybar wifi

--- polybar/bin/status_wifi.sh
+++ polybar/bin/status_wifi.sh
@@ -3,8 +3,10 @@
 IFACE=$(ip address show | grep "wlp")
 
 if [ "$?" -eq "0" ]; then
-    test $(echo $IFACE | grep inet) && echo "%{F#2495e7}直%{u-}"
-    ! test $(echo $IFACE | grep inet) && echo "%{F#FF0000}直%{u-}"
+    # interface disconnected
+    test $(echo $IFACE | grep inet | wc -l) -eq 0 && "%{F#FF0000}直%{u-}%{F-}"
+    # interface connected
+    test $(echo $IFACE | grep inet | wc -l) -gt 0 && echo "%{F#2495e7}直%{u-}%{F-}"
 else
     echo "%{F#555}睊%{u-}%{F-}"
 fi
