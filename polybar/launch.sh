0dfbb5289ded2599aad50676881e1c07579d41cc
procamora <pablojoserocamora@gmail.com>
2020-10-06 01:12:06 +0200

    polybar kill process when restart

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-killall -q polybar
+killall -q -9 polybar
+ps aux | grep "scripts/gmail/launch.py" | awk '{print $2}' | xargs kill -9 -q 2> /dev/null
+ps aux | grep "scripts/networkmanager_dmenu.py" | awk '{print $2}' | xargs kill -9 -q 2> /dev/null
 
 while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
 
@@ -12,7 +14,8 @@ while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
 #  polybar --reload my_config &
 #fi
 
-for m in $(polybar --list-monitors | cut -d":" -f1); do
+# filter monitor xrandr
+for m in $(polybar --list-monitors | grep -v xrandr | awk -F ":" '{print $1}'); do
     MONITOR=$m polybar --reload my_config &
 done
 
