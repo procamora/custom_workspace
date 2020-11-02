112eba595c10c986152f60b3e9603f317b81eeac
procamora <pablojoserocamora@gmail.com>
2020-11-02 16:03:46 +0100

    add timeout sudo

--- custom_workspace.sh
+++ custom_workspace.sh
@@ -435,8 +435,10 @@ function setup_tmux() {
 function install_bspwn() {
     setup_utils
     setup_bspwm
+    sudo -v # refresh timeout
     setup_fonts
     setup_polybar
+    sudo -v # refresh timeout
     setup_i3lock
 }
 
@@ -450,6 +452,7 @@ function install_vim() {
 function install_zsh() {
     setup_utils
     setup_fonts
+    sudo -v # refresh timeout
     setup_zsh
     setup_konsole
     setup_tmux
@@ -459,12 +462,16 @@ function install_zsh() {
 function install_all() {
     setup_utils 
     setup_bspwm
+    sudo -v # refresh timeout
     setup_fonts 
     setup_polybar 
+    sudo -v # refresh timeout
     setup_i3lock
     setup_vim
+    sudo -v # refresh timeout
     setup_zsh 
     setup_konsole
+    sudo -v # refresh timeout
     setup_tmux
 }
 
